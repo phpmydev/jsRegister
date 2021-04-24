5b73396755323abe3fbc727ae043c9e9e012e31d
Richard Whitney <richard@whitneys.co>
2021-04-23 19:47:51 -0700

    truncate error and startup logs

--- config.js
+++ config.js
@@ -20,19 +20,3 @@ module.exports = {
   dbpassword: '',
   database: 'yourdbname',
 }
-
-/*
- *
- create database jsRegister;
-
-CREATE TABLE jsRegister.`events` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) NOT NULL,
-  `start` datetime NOT NULL,
-  `end` datetime NOT NULL,
-  `amount` float NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
-*
- * */
-
